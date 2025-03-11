ea5e36d3fbb40e2b90292b5d2427915acff8a1bf
Coda Hale <coda.hale@gmail.com>
2025-03-10 21:35:26 -0600

    fix(static): improve static asset serving

--- internal/yellhole/static/static.go
+++ internal/yellhole/static/static.go
@@ -3,24 +3,32 @@ package static
 import (
 	"embed"
 	"fmt"
+	"io/fs"
 	"net/http"
 )
 
-//go:embed css js *.png *.ico *.webmanifest *.br *.gz
+//go:embed assets/*
 var assets embed.FS
 
 func Register(mux *http.ServeMux) {
-	fs := http.FileServerFS(assets)
-	dir, err := assets.ReadDir(".")
+	assets, err := fs.Sub(assets, "assets")
 	if err != nil {
 		panic(err)
 	}
 
-	for _, f := range dir {
-		if f.IsDir() {
-			mux.Handle(fmt.Sprintf("GET /%s/", f.Name()), fs)
-		} else {
-			mux.Handle(fmt.Sprintf("GET /%s", f.Name()), fs)
+	handler := http.FileServerFS(assets)
+	if err := fs.WalkDir(assets, ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
 		}
+
+		if d.IsDir() {
+			return nil
+		}
+
+		mux.Handle(fmt.Sprintf("GET /%s", path), handler)
+		return nil
+	}); err != nil {
+		panic(err)
 	}
 }
