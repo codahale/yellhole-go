4ee8a355aae65c8688c4b983f8779aa36fe786f2
Coda Hale <coda.hale@gmail.com>
2025-03-14 12:29:49 -0600

    feat(auth): add auth UI

--- auth.go
+++ auth.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/codahale/yellhole-go/config"
 	"github.com/codahale/yellhole-go/db"
+	"github.com/codahale/yellhole-go/view"
 )
 
 type authController struct {
@@ -19,7 +20,11 @@ func newAuthController(config *config.Config, queries *db.Queries) *authControll
 func (ac *authController) RegisterPage(w http.ResponseWriter, r *http.Request) {
 	// TODO check for existing passkey
 	// TODO ensure session isn't authenticated
-	http.NotFound(w, r)
+
+	w.Header().Set("content-type", "text/html")
+	if err := view.Render(w, "register.html", nil); err != nil {
+		panic(err)
+	}
 }
 
 func (ac *authController) RegisterStart(w http.ResponseWriter, r *http.Request) {
@@ -34,7 +39,10 @@ func (ac *authController) RegisterFinish(w http.ResponseWriter, r *http.Request)
 
 func (ac *authController) LoginPage(w http.ResponseWriter, r *http.Request) {
 	// TODO ensure session isn't authenticated
-	http.NotFound(w, r)
+	w.Header().Set("content-type", "text/html")
+	if err := view.Render(w, "login.html", nil); err != nil {
+		panic(err)
+	}
 }
 
 func (ac *authController) LoginStart(w http.ResponseWriter, r *http.Request) {
