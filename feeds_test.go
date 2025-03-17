9fbf88c349fea0bc603f703780441d8dbd382f11
Coda Hale <coda.hale@gmail.com>
2025-03-16 21:55:57 -0600

    test(feeds): add home page test

---
+++ feeds_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/codahale/yellhole-go/db"
+	"github.com/google/uuid"
+)
+
+func TestFeedsHome(t *testing.T) {
+	env := newTestApp(t)
+	defer env.teardown()
+
+	if err := env.app.queries.CreateNote(context.TODO(), db.CreateNoteParams{
+		NoteID: uuid.NewString(),
+		Body:   "It's a *test*.",
+	}); err != nil {
+		t.Fatal(err)
+	}
+
+	req := httptest.NewRequest("GET", "http://example.com/", nil)
+	w := httptest.NewRecorder()
+	env.ServeHTTP(w, req)
+
+	resp := w.Result()
+	body, _ := io.ReadAll(resp.Body)
+
+	if got, want := http.StatusOK, resp.StatusCode; got != want {
+		t.Errorf("status=%d, want=%d", got, want)
+	}
+
+	if want := "It's a <em>test</em>."; strings.Contains(string(body), want) {
+		t.Errorf("note not in body, want=%q", want)
+	}
+}
