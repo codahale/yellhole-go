d53bd3fcf0039b67d339db38a476cf9e7c60439b
Coda Hale <coda.hale@gmail.com>
2025-04-17 09:44:32 -0600

    test(markdown): improve test structure

--- markdown_test.go
+++ markdown_test.go
@@ -7,44 +7,46 @@ import (
 	"testing"
 )
 
-func TestHTML(t *testing.T) {
-	actual, err := markdownHTML("It's ~~not~~ _electric_!")
+func TestMarkdownHTML(t *testing.T) {
+	html, err := markdownHTML("It's ~~not~~ _electric_!")
 	if err != nil {
 		t.Fatal(err)
 	}
-	if expected := template.HTML("<p>It&rsquo;s <del>not</del> <em>electric</em>!</p>\n"); expected != actual {
-		t.Errorf("expected %q, but was %q", expected, actual)
+
+	if got, want := html, template.HTML("<p>It&rsquo;s <del>not</del> <em>electric</em>!</p>\n"); got != want {
+		t.Errorf("markdownHTML(s) = %q, want = %q", got, want)
 	}
 }
 
-func TestNoteDescription(t *testing.T) {
-	actual, err := markdownText("It's _electric_!\n\nBoogie woogie woogie.")
+func TestMarkdownText(t *testing.T) {
+	text, err := markdownText("It's _electric_!\n\nBoogie woogie woogie.")
 	if err != nil {
 		t.Fatal(err)
 	}
-	if expected := "It’s electric! Boogie woogie woogie."; expected != actual {
-		t.Errorf("expected %q, but was %q", expected, actual)
+
+	if got, want := text, "It’s electric! Boogie woogie woogie."; got != want {
+		t.Errorf("markdownText(s) = %q, want = %q", got, want)
 	}
 }
 
-func TestNoteImages(t *testing.T) {
+func TestMarkdownImages(t *testing.T) {
 	a, _ := url.Parse("/doink.png")
 	b, _ := url.Parse("http://example.com/cool.bmp")
 
-	actual, err := markdownImages(fmt.Sprintf("Hello!\n\n![](%s)\n\n![](%s)", a, b))
+	images, err := markdownImages(fmt.Sprintf("Hello!\n\n![](%s)\n\n![](%s)", a, b))
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if len(actual) != 2 {
-		t.Fatalf("expected 2 images but was %d", len(actual))
+	if got, want := 2, len(images); got != want {
+		t.Fatalf("len(images) = %d, want = %d", got, want)
 	}
 
-	if expected, actual := a.String(), actual[0].String(); expected != actual {
-		t.Errorf("expected %s but was %s", expected, actual)
+	if got, want := images[0].String(), a.String(); got != want {
+		t.Errorf("images[0].String() = %q, want = %q", got, want)
 	}
 
-	if expected, actual := b.String(), actual[1].String(); expected != actual {
-		t.Errorf("expected %s but was %s", expected, actual)
+	if got, want := images[1].String(), b.String(); got != want {
+		t.Errorf("images[1].String() = %q, want = %q", got, want)
 	}
 }
