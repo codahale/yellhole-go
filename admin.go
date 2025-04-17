6cc2caae171e01e81144351d8a03c0c8a1593d8a
Coda Hale <coda.hale@gmail.com>
2025-04-17 10:10:06 -0600

    feat(admin): check Sec-Fetch-Site to prevent CSRF

--- admin.go
+++ admin.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
 	"net/http"
 	"time"
 
@@ -31,6 +32,12 @@ func (ac *adminController) adminPage(w http.ResponseWriter, r *http.Request) {
 func (ac *adminController) newNote(w http.ResponseWriter, r *http.Request) {
 	if r.FormValue("preview") == fmt.Sprint(true) {
 		ac.templates.render(w, "preview.html", r.FormValue("body"))
+		return
+	}
+
+	if v := r.Header.Get("sec-fetch-site"); v != "same-origin" && v != "none" {
+		slog.Error("invalid sec-fetch-site value", "sec-fetch-site", v)
+		http.Error(w, "forbidden", http.StatusForbidden)
 		return
 	}
 
