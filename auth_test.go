8619a5d4b6bb375a376c1cf15a2d499cce1515f9
Coda Hale <coda.hale@gmail.com>
2025-05-29 21:27:25 -0600

    test(auth): add tests for login/register pages

--- auth_test.go
+++ auth_test.go
@@ -4,6 +4,10 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
+
+	"github.com/codahale/yellhole-go/internal/db"
+	"github.com/go-webauthn/webauthn/webauthn"
 )
 
 func TestRegisterPage(t *testing.T) {
@@ -19,5 +23,71 @@ func TestRegisterPage(t *testing.T) {
 
 	if got, want := resp.StatusCode, http.StatusOK; got != want {
 		t.Errorf("resp.StatusCode = %d, want = %d", got, want)
+	}
+}
+
+func TestRegisterPageWithCredential(t *testing.T) {
+	t.Parallel()
+
+	app := newTestApp(t)
+	if err := app.queries.CreateWebauthnCredential(t.Context(), &db.JSONCredential{
+		Data: &webauthn.Credential{ID: []byte("test-id")},
+	}, time.Now()); err != nil {
+		t.Fatal(err)
+	}
+
+	req := httptest.NewRequest("GET", "http://example.com/register", nil)
+	w := httptest.NewRecorder()
+	app.ServeHTTP(w, req)
+
+	resp := w.Result()
+
+	if got, want := resp.StatusCode, http.StatusSeeOther; got != want {
+		t.Errorf("resp.StatusCode = %d, want = %d", got, want)
+	}
+
+	if got, want := resp.Header.Get("location"), "http://example.com/login"; got != want {
+		t.Errorf("resp.Header.Get(\"location\") = %q, want = %q", got, want)
+	}
+}
+
+func TestLoginPage(t *testing.T) {
+	t.Parallel()
+
+	app := newTestApp(t)
+	if err := app.queries.CreateWebauthnCredential(t.Context(), &db.JSONCredential{
+		Data: &webauthn.Credential{ID: []byte("test-id")},
+	}, time.Now()); err != nil {
+		t.Fatal(err)
+	}
+
+	req := httptest.NewRequest("GET", "http://example.com/login", nil)
+	w := httptest.NewRecorder()
+	app.ServeHTTP(w, req)
+
+	resp := w.Result()
+
+	if got, want := resp.StatusCode, http.StatusOK; got != want {
+		t.Errorf("resp.StatusCode = %d, want = %d", got, want)
+	}
+}
+
+func TestLoginPageWithoutCredential(t *testing.T) {
+	t.Parallel()
+
+	app := newTestApp(t)
+
+	req := httptest.NewRequest("GET", "http://example.com/login", nil)
+	w := httptest.NewRecorder()
+	app.ServeHTTP(w, req)
+
+	resp := w.Result()
+
+	if got, want := resp.StatusCode, http.StatusSeeOther; got != want {
+		t.Errorf("resp.StatusCode = %d, want = %d", got, want)
+	}
+
+	if got, want := resp.Header.Get("location"), "http://example.com/register"; got != want {
+		t.Errorf("resp.Header.Get(\"location\") = %q, want = %q", got, want)
 	}
 }
