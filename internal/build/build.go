0877c6dfd1b368f89786c39391179ee276905205
Coda Hale <coda.hale@gmail.com>
2025-06-08 20:09:03 -0600

    refactor(build): fully handle errors

--- internal/build/build.go
+++ internal/build/build.go
@@ -3,19 +3,20 @@ package build
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"errors"
 	"fmt"
 	"io"
 	"os"
 )
 
 // Tag returns the truncated SHA-256 hash of the current executable.
-func Tag() (string, error) {
+func Tag() (tag string, err error) {
 	f, err := os.Open(os.Args[0])
 	if err != nil {
 		return "", fmt.Errorf("failed to open the current executable: %w", err)
 	}
 	defer func() {
-		_ = f.Close()
+		err = errors.Join(err, f.Close())
 	}()
 
 	h := sha256.New()
